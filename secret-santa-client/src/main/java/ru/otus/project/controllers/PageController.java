7ccf1967d7f8037c023769d584b43869f4bf5e2d
Anna-Yudina <anuta-nuty@yandex.ru>
2024-03-14 23:27:42 +0700

    Сделала главную страницу, добавила заготовку на форму, добилась нужных стилей css

--- secret-santa-client/src/main/java/ru/otus/project/controllers/PageController.java
+++ secret-santa-client/src/main/java/ru/otus/project/controllers/PageController.java
@@ -1,13 +1,22 @@
 package ru.otus.project.controllers;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@Slf4j
 public class PageController {
 
     @GetMapping("/secret")
     public String secretPage() {
+        System.out.println("зашли в метод secretPage");
         return "secret";
+    }
+
+    @GetMapping("/form")
+    public String formPage() {
+        log.info("зашли в метод formPage");
+        return "form";
     }
 }
